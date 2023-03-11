4b5f7cc0b31a1ce5bd2b33dfc2aee89d84b7958c
raidenkhan <96030339+raidenkhan@users.noreply.github.com>
2023-03-11 22:15:09 +0000

    Add files via upload

--- newAtm.c
+++ newAtm.c
@@ -8,26 +8,31 @@ int id;
 void readEntries(void);
 void makeWithdrawal(void);
 void createUser(void);
+void changePin(void);
 int main(){
 printf("\n\nWelcome\n\n");
 char cont='y';
 while(cont=='y'){
-    printf("\n1.Add a user \n2.Make A Deposit \n3.ReadData\n>>>:");
+    printf("\n1.Add a user \n2.Make A Withdrawal \n3.ReadData\n4.Change Pin\n>>>:");
     char choice;
     scanf(" %c",&choice);
     fflush(stdin);
+    //SWITCH INTO AVAILABLE USER OPTIONS
     switch(choice){
-case '1':
-    createUser();
-    break;
-case '2':
-    makeWithdrawal();
-    break;
-case '3':
-    readEntries();
-    break;
-default:
-    printf("Please enter A valid Choice to continue");
+                case '1':
+                    createUser();
+                    break;
+                case '2':
+                    makeWithdrawal();
+                    break;
+                case '3':
+                    readEntries();
+                    break;
+                case'4':
+                    changePin();
+                    break;
+                default:
+                    printf("Please enter A valid Choice to continue");
     }
     printf("\nWould you like to continue\n");
     scanf(" %c",&cont);
@@ -39,7 +44,7 @@ return 0;
 void createUser(){
 struct banker curr;
 FILE *fp;
-fp=fopen("newData.txt","w");
+fp=fopen("newData.txt","a");
 printf("Please enter name: ");
 scanf(" %s",curr.name);
 printf("\nPlease enter balance: ");
@@ -108,4 +113,49 @@ while(fread(&curr,sizeof(struct banker),1,fp)){
     printf("Name : %s \t Balance: %f \n with id %d",curr.name,curr.balance,curr.id);
 }
 fclose(fp);
+}
+
+
+
+void changePin(){
+printf("Please enter the old pin: ");
+int id =0;
+int newId=0;
+scanf("%d",&id);
+
+ struct banker curr;
+    struct banker toWrite;
+    int pres=0;
+    FILE *fp;
+    FILE *fp1;
+    fp=fopen("newData.txt","r");
+    fp1=fopen("tmpData.txt","w");
+if(fp==NULL|| fp1==NULL){
+    printf("\nCan't create or read file\n");
+}
+    while(fread(&curr,sizeof(struct banker),1,fp)){
+        if(curr.id==id){
+            printf("Welcome %s\n",curr.name);
+            pres=1;
+            printf("\nPlease enter your new Pin: ");
+            scanf("%d",&newId);
+            strcpy(toWrite.name,curr.name);
+            fflush(stdin);
+            toWrite.id=newId;
+            toWrite.balance=curr.balance;
+            fclose(fp1);
+            fp1=fopen("tmpData.txt","a");
+            fwrite(&toWrite,sizeof(struct banker),1,fp1);
+        }else{
+            fwrite(&curr,sizeof(struct banker),1,fp1);
+        }
+    }
+    printf("\nThis is pres %d\n",pres);
+    if(!pres){
+        printf("No user with this id!!!\n");
+    }
+    fclose(fp1);
+    fclose(fp);
+    remove("newData.txt");
+    rename("tmpData.txt","newData.txt");
 }
